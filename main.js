9f42e9e92cd3aaf2799885f7ad5ed0592528d852
prem <prem.sherpa@codeimmersives.com>
2020-07-01 15:24:59 -0400

    almost there

--- main.js
+++ main.js
@@ -1,10 +1,66 @@
 /******************
  * YOUR CODE HERE *
 ******************/
+const doubleAll = function(arr){
+  let result = [];
+  for ( let i = 0; i < arr.length; i++){
+    result.push(arr[i] * 2)
+  }
+  
+  return result;
+}
+
+//2
+const yelledGreetings = function(str){
+  let result = [];
+  for ( let i = 0; i < str.length; i++){
+    result.push(str[i]+'!');
+  }
+  return result;
+}
 
 
 
+//3
+const absoluteValues = function(arr){
+  let result = [];
+  for ( let i = 0; i < arr.length; i++){
+    result.push(Math.abs(arr[i]));
+  }
 
+  return result;
+}
+
+// //4..in a littler bit
+
+const upperCaseFirstLetters = function(arr){
+  const result = [];
+  for ( let i = 0; i < arr.length; i++){
+    let allLowerCase = arr[i].toLowerCase();
+    // console.log(allLowerCase);
+    for(let i = 1; i<allLowerCase.length; i++){
+      let result =allLowerCase[0].toUpperCase();
+      result += allLowerCase[i]
+      // console.log(result)
+    }
+
+ 
+}return result;
+}
+
+upperCaseFirstLetters(['my','park'])
+
+//5
+// const changeToInitials = function(arr){
+//   let result = [];
+//   for ( let i = 0; i < arr.length; i++){
+    
+//   }
+
+//   return result;
+// }
+
+// changeToInitials(['conlin jafa','prem sherp'])
 
 
 /*******************************************
